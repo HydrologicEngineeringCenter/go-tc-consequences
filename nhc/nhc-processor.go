7fe87f6772db5b55b49947400dab531354d73dad
Will Lehman <william.p.lehman@usace.army.mil>
2021-02-02 16:02:14 -0800

    adding byte converter

---
+++ nhc/nhc-processor.go
@@ -0,0 +1,22 @@
+package nhc
+
+func convertByteToDepth(b byte) float64 {
+	switch b {
+	case 1:
+		return 1.0
+	case 2:
+		return 2.0
+	case 3:
+		return 3.0
+	case 4:
+		return 6.0
+	case 5:
+		return 9.0
+	case 7:
+		return 0.0 //leveed area
+	case 15:
+		return 0.0 //intertidal mask only, may experiance high tide or estuarine class in nlcd?
+	default:
+		return 0.0 //
+	}
+}
