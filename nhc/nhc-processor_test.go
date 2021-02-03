f7101bbdaafce10eda4f62195c2bc98f6d634943
Will Lehman <william.p.lehman@usace.army.mil>
2021-02-02 19:23:40 -0800

    fixing tests to cast to depth event.

--- nhc/nhc-processor_test.go
+++ nhc/nhc-processor_test.go
@@ -2,27 +2,34 @@ package nhc
 
 import (
 	"testing"
+
+	"github.com/USACE/go-consequences/hazards"
 )
 
 func Test_Convert(t *testing.T) {
-	d := convertDepthtoDepthEvent(convertByteToDepth(1))
-	if d.Depth != 1.0 {
+	d := convertDepthtoHazardEvent(convertByteToDepth(1))
+	de, _ := d.(hazards.DepthEvent)
+	if de.Depth != 1.0 {
 		t.Errorf("expected 1.0 got something else.")
 	}
-	d2 := convertDepthtoDepthEvent(convertByteToDepth(2))
-	if d2.Depth != 2.0 {
+	d2 := convertDepthtoHazardEvent(convertByteToDepth(2))
+	de2, _ := d2.(hazards.DepthEvent)
+	if de2.Depth != 2.0 {
 		t.Errorf("expected 2.0 got something else.")
 	}
-	d3 := convertDepthtoDepthEvent(convertByteToDepth(3))
-	if d3.Depth != 3.0 {
+	d3 := convertDepthtoHazardEvent(convertByteToDepth(3))
+	de3, _ := d3.(hazards.DepthEvent)
+	if de3.Depth != 3.0 {
 		t.Errorf("expected 3.0 got something else.")
 	}
-	d4 := convertDepthtoDepthEvent(convertByteToDepth(4))
-	if d4.Depth != 6.0 {
+	d4 := convertDepthtoHazardEvent(convertByteToDepth(4))
+	de4, _ := d4.(hazards.DepthEvent)
+	if de4.Depth != 6.0 {
 		t.Errorf("expected 6.0 got something else.")
 	}
-	d5 := convertDepthtoDepthEvent(convertByteToDepth(5))
-	if d5.Depth != 9.0 {
+	d5 := convertDepthtoHazardEvent(convertByteToDepth(5))
+	de5, _ := d5.(hazards.DepthEvent)
+	if de5.Depth != 9.0 {
 		t.Errorf("expected 9.0 got something else.")
 	}
 }
