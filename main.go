98b35375dc0bba93891c391cdfcfafbe2bc85d77
Will Lehman <william.p.lehman@usace.army.mil>
2021-06-18 22:00:31 -0400

    vertical datum

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	//command line program
 	var sp consequences.StreamProvider
 	var hp hazardproviders.HazardProvider //not sure what it will be yet, but we can declare it!
+	hazardProviderVerticalUnitsIsFeet := true
 	var ow consequences.ResultsWriter     //need a file path to write anything...
 
 	//-ss (structures source) -sfp (structure file path)  -hs (hazard source) -hfp (hazard file path) -ot (output type) //we will define the path internally?
@@ -23,6 +24,7 @@ func main() {
 	ss := flag.String("ss", "nsi", "structure source, (optional), acceptable terms: nsi(default), gpkg, shp")
 	hfp := flag.String("hfp", "", "hazard file path, (required)")
 	hs := flag.String("hs", "", "hazard source, (required), acceptable terms: nhc, depths")
+	ht := flag.String("ht", "feet","hazard type of vertical datum, (optional), acceptable terms: feet (default), meters")
 	ot := flag.String("ot", "gpkg", "output type, (optional), acceptable terms: gpkg (default), shp, geojson, summaryDollars, summaryDepths")
 
 	flag.Parse()
@@ -42,12 +44,26 @@ func main() {
 	}
 	//fmt.Println(hfp)
 	//fmt.Println(*hfp)
+	if *ht != ""{
+		switch *ht {
+		case "feet":
+			hazardProviderVerticalUnitsIsFeet = true
+		case "meters":
+			hazardProviderVerticalUnitsIsFeet = false
+		}
+	} else {
+		panic("cannot compute without hazard provider path, use -h for help.")
+	}
 	if *hfp != "" {
 		switch *hs {
 		case "nhc":
 			hp = nhc.Init(*hfp)
 		case "depths":
-			hp = hazardproviders.Init(*hfp)
+			if hazardProviderVerticalUnitsIsFeet{
+				hp = hazardproviders.Init(*hfp)
+			}else{
+				hp = hazardproviders.Init_Meters(*hfp)
+			}
 		}
 	} else {
 		panic("cannot compute without hazard provider path, use -h for help.")
